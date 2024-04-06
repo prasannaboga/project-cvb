119cf7b55e384db9065325b04b23a77915990dd7
Prasanna Kumar <prasannaboga@gmail.com>
2024-04-06 16:36:13 +0530

    users api

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from project_ai_gemini.config.settings import Settings
+from project_cvb.config.settings import Settings
+from project_cvb.app import users
+
 
 app = FastAPI()
+app.include_router(users.router, prefix="/users")
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
+
 settings = Settings()
+
 
 @app.get("/")
 async def root():
