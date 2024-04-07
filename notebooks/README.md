5a7ef41b3976042336778e27bcef804807ac229d
Prasanna Kumar <prasannaboga@gmail.com>
2024-04-07 10:01:40 +0530

    google generativeai files

---
+++ notebooks/README.md
@@ -0,0 +1,4 @@
+### Ref links
+
+https://ai.google.dev/tutorials/python_quickstart
+https://www.youtube.com/watch?v=DJtX3S7qx2s
--- project_cvb/config/settings.py
+++ project_cvb/config/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
   ENVIRONMENT: str
+  GEMINI_API_KEY: str
 
   class Config:
     env_file = ".env"
